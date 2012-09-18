e5521dbecd9353dd78dbb9e80ee51e01596179c0
Nicolas Duvollet <duvolletn@HP-Folio-13-Notebook-PC-1.(none)>
2012-09-18 09:55:36 +0200

    New files

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>Mini-site</title>
+	</head>
+	<body>
+		<div id='head'>
+			
+		</div>
+		<div id='corp'>
+			
+		</div>
+		<div id='foot'>
+			
+		</div>
+	</body>
+</html>
